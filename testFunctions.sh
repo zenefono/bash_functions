5b0fc15b84ffba3ad5fad6630f94cb5bb49b69a5
zenefono <94757402+zenefono@users.noreply.github.com>
2021-11-26 00:05:58 +0100

    Update testFunctions.sh

--- testFunctions.sh
+++ testFunctions.sh
@@ -15,6 +15,13 @@ dockerStopAllContainers() {
 }
 
 genAndAddSshKey() {
+	# da: https://docs.github.com/en/authentication/connecting-to-github-with-ssh
+	# Lists the files in your .ssh directory, if they exist
+	if ls -al ~/.ssh 2> /dev/null; then
+		echo "TODO! selezionare la chiave ssh"
+	else
+		echo "NON esiste!"
+
 		# Generating a new SSH key
 		ssh-keygen -t ed25519 -C "${gitUsrEmail}"
 
@@ -25,6 +32,8 @@ genAndAddSshKey() {
 		echo ""
 		echo "Select and copy the contents of the id_ed25519.pub file"
 		echo "Go to https://github.com/settings/keys and add it to GitHub"
+
+	fi
 }
 
 
