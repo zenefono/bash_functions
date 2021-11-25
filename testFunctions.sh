dd390e7e65c99e0a50d7cfa2215915f592751b66
zenefono <94757402+zenefono@users.noreply.github.com>
2021-11-25 17:17:46 +0100

    Update testFunctions.sh

--- testFunctions.sh
+++ testFunctions.sh
@@ -1,3 +1,24 @@
+includeLocalFile(){
+	DIR="${BASH_SOURCE%/*}"
+	if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+	. "$DIR/$@" --source-only
+}
+includeLocalFile gitFunctions.sh
+
 prova(){
   echo "ciao io sono una funzione di prova"
 }
+
+
+## MAIN LOOP FOR FILE OF FUNCTIONS TO INCLUDE
+main() {
+	moduleOK() {
+		echo "# modulo ${BASH_SOURCE} caricato"
+	}
+
+	if [ "${1}" != "--source-only" ]; then
+		moduleOK "${@}"
+	fi
+}
+main "${@}"
