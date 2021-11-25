182fd78025c476a8259558fbfc015df44b3d80d8
zenefono <94757402+zenefono@users.noreply.github.com>
2021-11-26 00:01:20 +0100

    Update testFunctions.sh
    ADD genAndAddSshKey

--- testFunctions.sh
+++ testFunctions.sh
@@ -14,6 +14,20 @@ dockerStopAllContainers() {
 	docker kill $(docker ps -q)
 }
 
+genAndAddSshKey() {
+		# Generating a new SSH key
+		ssh-keygen -t ed25519 -C "${gitUsrEmail}"
+
+		# Adding your SSH key to the ssh-agent
+		eval "$(ssh-agent -s)"
+		ssh-add ~/.ssh/id_ed25519
+		cat ~/.ssh/id_ed25519.pub
+		echo ""
+		echo "Select and copy the contents of the id_ed25519.pub file"
+		echo "Go to https://github.com/settings/keys and add it to GitHub"
+}
+
+
 ## MAIN LOOP FOR FILE OF FUNCTIONS TO INCLUDE
 main() {
 	moduleOK() {
